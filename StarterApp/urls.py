3a5f7f8f7a7084de424ae79cb74877836176eb90
Agostinasofialonso <agostialonso1999@gmail.com>
2023-08-02 20:08:16 -0300

    Intento

--- StarterApp/urls.py
+++ StarterApp/urls.py
@@ -1,8 +1,8 @@
 
-from django.contrib import admin
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from StarterApp import views
+from . import views
 from django.views.generic import TemplateView 
 
 app_name= "StarterApp"
@@ -13,10 +13,6 @@ urlpatterns = [
     path('dogs/create/', views.createdogs, name='createdogs'),
     path('birds/create/', views.createbirds, name='createbirds'),
     path('search/', views.search, name='search'),
-]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', TemplateView.as_view(template_name='start/start.html'), name='start'),
 ]
