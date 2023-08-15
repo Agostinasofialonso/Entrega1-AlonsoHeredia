00d63b5b8ee020e58d99738f1035ecd48dbbcfad
Agostinasofialonso <agostialonso1999@gmail.com>
2023-08-15 19:07:02 -0300

    viewsok

--- user_app/urls.py
+++ user_app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("register/", views.register, name='register'),
     path("login/", views.iniciar_sesion, name='login'),
     path("logout/", LogoutView.as_view(template_name='login/logout.html'), name='logout'),
-    path("profile/", views.verPerfil, name='profile'),
     path("profile/edit/", views.editar_perfil, name='editprofile'),
     path("password/edit/", views.editpassword.as_view(), name='editpassword'),
-     path('profile/', views.verPerfilView.as_view(), name='profile'),
+    path('profile/', views.VerPerfilView.as_view(), name='profile'),
+
 ]
