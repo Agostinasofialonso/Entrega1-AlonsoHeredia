2f6e2fad35922ea62296e3cdb0b8d555d3aa54e5
Agostinasofialonso <agostialonso1999@gmail.com>
2023-08-15 21:22:46 -0300

    admin

--- StarterApp/admin.py
+++ StarterApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cats, Dogs, Birds
+
+admin.site.register(Cats)
+admin.site.register(Dogs)
+admin.site.register(Birds)
+
